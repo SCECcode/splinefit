cd97dae8c492831e6c88f43a3b1972e505c7a85b
Ossian O'Reilly <ooreilly@stanford.edu>
2017-10-27 23:56:29 -0700

    Fix command parser and add documentation.

--- splinefit/gmsh.py
+++ splinefit/gmsh.py
@@ -1,35 +1,56 @@
-# Spline\((\d+)\)\s*=\s*\{2,(\sp\d+\s\+\s\d+,)
-
-supportedcmds = {'Spline', 'Point', 'Line Loop','Ruled Surface'} 
-
-class Command:
-
-    def __init__(self, cmd, data):
-
-        self.cmd = cmd
+supportedcmds = {'Spline', 'Point', 'Line Loop', 'Ruled Surface'} 
 
 def get_command(cmd, geo):
+    """
+    Searches through a gmsh geo script for a specific command and extracts
+    all occurences found. 
+    A gmsh command uses the syntax: `command(id) = {.., .., parameters, etc};`
+
+    Parameters
+
+    cmd : string,
+          The command to search for
+    geo : string,
+          The input geo-script to parse. 
+
+    Returns
+
+    out : dict,
+          All of the instances found of the command. 
+          The command id is used as the key and a list of parameters
+          is the value. If no instance of `cmd` is found, then this function
+          returns `None`.
+
+    # Examples
+
+    >>> geo = "Point(1) = {1.0, 2.0, 3.0};"
+    >>> point = get_command('Point', geo)
+    >>> point
+    {'1': ['1.0', '2.0', '3.0']}
+
+    """
+
     import re 
 
     if cmd not in supportedcmds:
         raise KeyError("Command: %s not supported" %cmd)
 
-    pattern = '%s\((.*)\)\s*=\s*\{(.*)\};'%cmd
+    pattern = r'%s\((.*)\)\s*=\s*\{(.*)\}\;.*'%cmd
     p = re.compile(pattern)
 
-    matches = p.findall(geo, re.M)
+    matches = p.findall(geo)
 
     data = {}
     if len(matches) == 0:
         return None
     
-    cmd = {}
+    out = {}
 
     for m in matches:
         data = m[1].strip().split(', ')
-        cmd[m[0]] = data
+        out[m[0]] = data
 
-    return cmd
+    return out
 
 
 def get_variables(var, geo):
@@ -37,7 +58,37 @@ def get_variables(var, geo):
 
 
 def check_groupmembers(group, members):
+    """
+    Determines if all the parameters in one or more groups exist in a list containing
+    the parameter IDs.
+
+    Parameters
+
+    group : dictionary,
+            A group is an item in the dictionary where the key in the dictionary
+            is the group ID and the value are the group members that are specified 
+            as a list.
+
+    Returns 
+
+    out : bool,
+          If all of the items in `members` are found in any group of
+          `groups` then this function returns True, and False otherwise.
+
+
+    # Examples
+
+    >>> points = {'p0' : [0.0, 0.0], 'p1' : [1.0, 2.0], 'p2' : [2.0, 3.0] }
+    >>> splines = {'0' : ['p0', 'p1', 'p2'], '1' : ['p1', 'p2'] }
+    >>> check_groupmembers(splines, points)
+    True
+
+    """
+
     from six import itervalues
+    out = True
     for val in itervalues(group):
         for v in val:
-            assert v in members
+            if not v in members:
+                out = False
+    return out
--- splinefit/tests/test_gmsh.py
+++ splinefit/tests/test_gmsh.py
@@ -1,17 +1,33 @@
 from splinefit import gmsh
 
 def load(test):
+
     txt = open('fixtures/' + test + '.geo').read() 
     return txt
 
 def test_get_command():
+
+    geo = "Point(1) = {1.0, 2.0, 3.0};"
+    point = gmsh.get_command('Point', geo)
+    assert point['1'][0] == '1.0'
 
     tests = ['test1']
 
     for test in tests:
         splines = gmsh.get_command('Spline', load(test))
         points  = gmsh.get_command('Point', load(test))
-        gmsh.check_groupmembers(splines, points)
+        assert gmsh.check_groupmembers(splines, points)
+
+def test_check_group():
+
+    a = {0 : [0], 1 : [1]}
+    b = {0 : 1, 1 : 1}
+    assert gmsh.check_groupmembers(a, b)
+    b = [0, 1]
+    assert gmsh.check_groupmembers(a, b)
+    a = {0 : [0], 1 : [1, 2]}
+    assert not gmsh.check_groupmembers(a, b)
+
 
     
 
