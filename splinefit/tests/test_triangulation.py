3990bf3f4d65f3b8db3ce052b67876b38dba37b2
ooreilly <ooreilly@stanford.edu>
2018-09-02 00:02:38 -0700

    Add some triangulation helper functions.

--- splinefit/tests/test_triangulation.py
+++ splinefit/tests/test_triangulation.py
@@ -20,16 +20,35 @@ def test_tri_to_edges():
     # Not a triangle
     with pytest.raises(Exception) : sf.triangulation.tri_to_edges([0,1])
 
+def test_edges_shared_tri_count():
+    tri = [1,2,3]
+    tris = np.array([[1,2,3],[2,3,4]]).astype(np.int64)
+    count_ans = [1, 2, 1, 1, 1]
+    edges = sf.triangulation.tris_to_edges(tris)
+    count = sf.triangulation.edges_shared_tri_count(edges)
+    assert np.all(np.equal(count,count_ans))
+
+def test_edges_to_nodes():
+    tri = [1,2,3]
+    tris = np.array([[1,2,3],[2,3,4]]).astype(np.int64)
+    nodes_ans = np.array([[1, 2], [2, 3], [1, 3], [3, 4], [2, 4]]).astype(np.int64)
+    edges = sf.triangulation.tris_to_edges(tris)
+    nodes = sf.triangulation.edges_to_nodes(edges)
+    assert np.all(np.equal(nodes,nodes_ans))
+
+
 def test_edge_mapping():
     assert sf.triangulation.edge_mapping(2,1) == '1-2'
     assert sf.triangulation.edge_mapping(1,2) == '1-2'
-
-    # Nodes in an edge must be unique
-    with pytest.raises(Exception) : sf.triangulation.edge_mapping(1,1)
     
     # Nodes must be int
     with pytest.raises(Exception) : sf.triangulation.edge_mapping('1',1)
     with pytest.raises(Exception) : sf.triangulation.edge_mapping(1,'1')
+
+def test_edge_inverse_mapping():
+    key = '1-2'
+    assert sf.triangulation.edge_inverse_mapping(key, True) == (1,2)
+    assert sf.triangulation.edge_inverse_mapping(key, False) == (2,1)
 
 def test_edge_reorder():
     assert sf.triangulation.edge_reorder((2,1)) == (1,2)
@@ -42,5 +61,11 @@ def test_is_node():
     assert not sf.triangulation.is_node(1.0)
     # Negative node ids not allowed
     assert not sf.triangulation.is_node(int(-1))
+
+def test_is_edge():
+    assert sf.triangulation.is_edge((int(1),int(10)))
+    assert not sf.triangulation.is_edge((int(1),int(1)))
+    assert not sf.triangulation.is_edge((int(1),int(1),int(2)))
+
 
 
--- splinefit/triangulation.py
+++ splinefit/triangulation.py
@@ -47,6 +47,52 @@ def tris_to_edges(tris):
                 edges[key]['triangles'].append(tri_id)
     return edges
 
+def edges_shared_tri_count(edges):
+    """
+    Return an array containing the number of triangles each edge belongs to.
+
+    Arguments:
+        edges : Edge data structure.
+
+    """
+    num_edges = len(edges)
+    count = np.zeros((num_edges,))
+    for key in edges:
+        edge = edges[key]
+        count[edge['id']] = len(edge['triangles'])
+    return count
+
+def edges_to_nodes(edges):
+    """
+    Return an array containing the nodes of each edge.
+
+    Arguments:
+        edges : Edge data structure.
+
+    """
+    num_edges = len(edges)
+    nodes = np.zeros((num_edges,2)).astype(np.int64)
+    for i, key in enumerate(edges):
+        ids = edge_inverse_mapping(key)
+        nodes[i,0] = ids[0]
+        nodes[i,1] = ids[1]
+    return nodes
+
+
+
+def unordered_boundary_edges(edges, tri_count, boundary_count=0):
+    """
+    Return an array containing the edges lie on the boundary. No particular
+    ordering of the boundary edges is attempted. 
+
+    Arguments:
+        edges : Edge data structure.
+        tri_count : Count  
+        boundary_count(optional) : determines the count an edge must have to be
+        classified as a boundary edge.
+
+    """
+    pass
 
 def tri_to_edges(tri):
     """
@@ -66,13 +112,9 @@ def edge_reorder(edge):
     first.
     """
     import numpy as np
-    id1 = edge[0]
-    id2 = edge[1]
-
-    assert is_node(id1) and is_node(id2)
-    assert id1 != id2
-    min_id = min(id1, id2)
-    max_id = max(id1, id2)
+    assert is_edge(edge)
+    min_id = min(edge[0], edge[1])
+    max_id = max(edge[0], edge[1])
     return (min_id, max_id)
 
 def edge_mapping(id1, id2):
@@ -82,6 +124,25 @@ def edge_mapping(id1, id2):
     """
     return "%s-%s" % edge_reorder((id1, id2))
 
+def edge_inverse_mapping(key, orientation=True):
+    """
+    Reverse the edge mapping. That is, convert the string "1-2" to (1,2) or
+    (2,1) depending on what the original orientation of the nodes in the edge
+    was.
+
+    Arguments:
+        key : String defining the edge mapping to reverse.
+        orientation : Orientation of nodes in the edge. Set to `False` if the
+        ordering should be reversed.
+
+    """
+    ids = key.split('-')
+    assert len(ids) == 2
+    ids = (int(ids[0]), int(ids[1]))
+    if orientation:
+        return ids
+    else:
+        return (ids[1], ids[0])
 
 def is_node(idx):
     """
@@ -95,3 +156,13 @@ def is_node(idx):
         return False
     return correct_type and nonnegative
 
+def is_edge(edge):
+    """
+    Check that the input satsifies the constraints that the define an edge.
+    """
+    correct_len =  len(edge) == 2
+    id1 = edge[0]
+    id2 = edge[1]
+    unique_ids = id1 != id2
+
+    return is_node(id1) and is_node(id2) and unique_ids
