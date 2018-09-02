40ea2f9fcdc2ca71c0750556e1893ce359cbf840
ooreilly <ooreilly@stanford.edu>
2018-09-01 22:05:51 -0700

    Add edge reordering function, and start construction of edge-based data structure.

--- splinefit/tests/test_triangulation.py
+++ splinefit/tests/test_triangulation.py
@@ -5,16 +5,23 @@ def test_edge_mapping():
     assert sf.triangulation.edge_mapping(2,1) == '1-2'
     assert sf.triangulation.edge_mapping(1,2) == '1-2'
 
-    # Nodes in a edge must be unique
+    # Nodes in an edge must be unique
     with pytest.raises(Exception) : sf.triangulation.edge_mapping(1,1)
     
     # Nodes must be int
     with pytest.raises(Exception) : sf.triangulation.edge_mapping('1',1)
     with pytest.raises(Exception) : sf.triangulation.edge_mapping(1,'1')
 
+def test_edge_reorder():
+    assert sf.triangulation.edge_reorder((2,1)) == (1,2)
+    assert sf.triangulation.edge_reorder((1,2)) == (1,2)
+
 def test_tri_to_edges():
     tri = [1,2,3]
     assert sf.triangulation.tri_to_edges(tri)[0] == (1,2)
     assert sf.triangulation.tri_to_edges(tri)[1] == (2,3)
     assert sf.triangulation.tri_to_edges(tri)[2] == (3,1)
+
+    # Not a triangle
+    with pytest.raises(Exception) : sf.triangulation.tri_to_edges([0,1])
 
--- splinefit/triangulation.py
+++ splinefit/triangulation.py
@@ -3,7 +3,7 @@ Module for querying and manipulating triangulations
 
 """
 
-def tris_to_edges(tri):
+def tris_to_edges(tris):
     """
     Construct an edge to element data structure for a given triangulation.
     The edge element data structure defines a mapping between an edge (N_1, N_2)
@@ -13,7 +13,7 @@ def tris_to_edges(tri):
     with the least index. 
     
     Arguments:
-        tri : Triangulation in the form of a m x 3 array.
+        tris : Triangulation in the form of a m x 3 array.
 
     Returns:
         edges : The edge-to-element data structure as explained above.
@@ -21,6 +21,15 @@ def tris_to_edges(tri):
     """
 
     edges = {}
+
+    #for idx, tri in enumerate(tris):
+    #    tri_edges = tri_to_edges(tri)
+    #    for edge_i in tri_edges:
+    #        key = edge_mapping(edge_i)
+    #        if key not in tri_edges:
+    #            edges[key] = {'orientation' : [], 'triangles' : []}
+    #            edges[key]['orientation'].append(
+
 
 def tri_to_edges(tri):
     """
@@ -31,17 +40,28 @@ def tri_to_edges(tri):
     Returns:
         A list of three tuples defining the edges of the given triangle.
     """
+    assert len(tri) == 3
     return [(tri[0], tri[1]), (tri[1], tri[2]), (tri[2], tri[0])]
+
+def edge_reorder(edge):
+    """
+    Reorder the nodes in an edge so that the node with the least index appears
+    first.
+    """
+    id1 = edge[0]
+    id2 = edge[1]
+
+    assert isinstance(id1, int)
+    assert isinstance(id2, int)
+    assert id1 != id2
+    min_id = min(id1, id2)
+    max_id = max(id1, id2)
+    return (min_id, max_id)
 
 def edge_mapping(id1, id2):
     """
     Define the mapping function for the edge-based data structure.
 
     """
-    assert isinstance(id1, int)
-    assert isinstance(id2, int)
-    assert id1 != id2
-    min_id = min(id1, id2)
-    max_id = max(id1, id2)
-    return "%s-%s" % (min_id, max_id)
+    return "%s-%s" % edge_reorder((id1, id2))
 
