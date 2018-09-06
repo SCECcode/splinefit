56557fb181e864cdde1ed83ae0e45ac6dbb0c06f
ooreilly <ooreilly@stanford.edu>
2018-09-06 00:10:36 -0700

    Fix constant.

--- splinefit/i3l.py
+++ splinefit/i3l.py
@@ -3,29 +3,43 @@ Implementation of the 3L algorithm
 
 The 3L Algorithm for Fitting Implicit
 Polynomial Curves and Surfaces to Data
-Michael M. Blane, Zhibin Lei, Member, IEEE, Hakan CË ivi, and David B. Cooper, Fellow, IEEE
-IEEE TRANSACTIONS ON PATTERN ANALYSIS AND MACHINE INTELLIGENCE, VOL. 22, NO. 3, MARCH 2000
+Michael M. Blane, Zhibin Lei, Member, IEEE, Hakan CË ivi, and David B. Cooper,
+Fellow, IEEE
+IEEE TRANSACTIONS ON PATTERN ANALYSIS AND MACHINE INTELLIGENCE, VOL. 22, NO. 3,
+MARCH 2000
 
 """
 import numpy as np
 
 def vander2d(p, deg):
     """
-    Construct Vandermonde-like matrix
+    Construct Vandermonde-like matrix that arises from treating the coefficients 
+    a_ij as unknowns when representing the bivariate polynomial graph
 
-    m = [1 x y]
+    f(x,y) = sum_ij a_ij x^i y^j,  i + j < deg + 1
+           = m*a
+
+    For deg = 2, coefficients are ordered as:
     m = [1 x x**2 y x*y y**2]
-       = [(0,0) (1,0) (2,0) (1,1) (0,1), (0,2)]
-
-
          (0,0) (1,0) (2,0)
          (0,1) (1,1) 
-         (0,2)
-
+         (0,2) 
+         ] 
+    and for deg = 3, 
        = [(0,0) (1,0) (2,0) (3,0)
           (0,1) (1,1) (2,1) 
           (0,2) (1,2) 
-          (0,3)
+          (0,3)]
+    and so forth.
+
+    Arguments:
+        p : Array of points (size: num pts x 2)
+        deg : Degree of bivariate polynomial
+
+    Returns:
+        M : Vandermonde-like matrix. M[0,0] = 1, M[0,1] = x_i (the x-coordinate
+            of the first point).
+
     """
     npts = p.shape[0]
 
@@ -63,5 +77,8 @@ def solve(M0, Mp, Mm, c):
     M = np.vstack((Mp, M0, Mm))
     e = np.ones((M0.shape[0],1))
     b = np.vstack((c*e, 0*e, -c*e))
-    coef = np.linalg.lstsq(M, b, rcond=None)
+    coef = np.linalg.lstsq(M, b, rcond=False)
     return coef
+
+# f(x,y) = c
+# a1 + a2*x + a3*x^2 + a4*y + a5*x*y + a6*y**2
--- splinefit/tests/test_i3l.py
+++ splinefit/tests/test_i3l.py
@@ -26,21 +26,23 @@ def test_vander2d():
     assert np.isclose(M[0,5],1   )  # y^2
 
 def test_solve():
-    w = 1.0
-    h = 1.0
-    deg = 4
-    c = 0.1
-    eps = 1e-1
-    p0 = parameteric_rectangle(w, h)
-    pp = parameteric_rectangle(w+eps, h+eps)
-    pm = parameteric_rectangle(w-eps, h-eps)
+    w = 1
+    h = 1
+    deg = 2
+    c = 0.02*w*h
+    eps = 0.5*c
+    p0 = parameteric_ellipse(w, h)
+    pp = parameteric_ellipse(w+eps, h+eps)
+    pm = parameteric_ellipse(w-eps, h-eps)
     M0 = sf.i3l.vander2d(p0, deg)
     Mp = sf.i3l.vander2d(pp, deg)
     Mm = sf.i3l.vander2d(pm, deg)
     coef = sf.i3l.solve(M0, Mp, Mm, c)[0]
     
-    x = np.linspace(-0.5, 0.5, 20)
-    y = np.linspace(-0.5, 0.5, 20)
+    nx = 100
+    ny = 100
+    x = np.linspace(-1.0, 1, nx)
+    y = np.linspace(-1.0, 1, ny)
     x, y = np.meshgrid(x, y)
 
     x_ = x.flatten()
@@ -49,23 +51,39 @@ def test_solve():
     M0 = sf.i3l.vander2d(pts, deg)
     
     src = M0.dot(coef)
-    levels=[1e-2]
-    Z = src.reshape(20, 20)
+    levels=[1e-6]
+    Z = src.reshape(nx, ny)
     import matplotlib.pyplot as plt
-    plt.contour(x, y, Z, [levels])
+    #plt.plot(p0[:,0],p0[:,1],'b-')
+    #plt.plot(pp[:,0],pp[:,1],'b-')
+    #plt.plot(pm[:,0],pm[:,1],'b-')
+    levels = [0.0]
+    plt.contour(x, y, Z, levels)
     plt.show()
-    assert 0
 
 def parameteric_rectangle(w, h, npts=100):
     # w: width, h : height
     u = np.linspace(-2*np.pi, np.pi*2, npts)
     x = 0.5*w*np.sign(np.cos(u))
     y = 0.5*h*np.sign(np.sin(u))
-    x = 0.5*w*np.cos(u)
-    y = 0.5*h*np.sin(u)
+    return np.stack((x, y)).T
+
+def parameteric_ellipse(w, h, npts=100):
+    # w: width, h : height
+    u = np.linspace(-np.pi, np.pi, npts)
+    x = w*np.cos(u)
+    y = h*np.sin(u)
+    return np.stack((x, y)).T
+
+def parameteric_fun(w, h, npts=100):
+    # w: width, h : height
+    u = np.linspace(-np.pi, np.pi, npts)
+    x = w*(1 + np.cos(u))*np.cos(u) - 0.5*w
+    y = h*(1 + np.cos(u))*np.sin(u) - 0.5*h
     return np.stack((x, y)).T
 
 def test_parameteric_rectangle():
+    return
     import matplotlib.pyplot as plt
     pts = parameteric_rectangle(1,1)
     plt.plot(pts[:,0],pts[:,1])
