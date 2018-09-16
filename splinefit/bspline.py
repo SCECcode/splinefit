be723728ed87d45c89e3012cec257d6936920719
ooreilly <ooreilly@stanford.edu>
2018-09-15 18:05:14 -0700

    Plot BSpline curve.

--- splinefit/bspline.py
+++ splinefit/bspline.py
@@ -1,8 +1,17 @@
 import numpy as np
 
-def basis(t):
+def cubic(t):
     """
-    Evaluate C^2 continuous cubic BSpline basis in each cell i=0,1,2,3.
+    Evaluate C^2 continuous cubic BSpline basis for each function B0, B1, B2, B3.
+
+    out = [t^3, t^2, t, 1] * B
+
+    To compute the Bspline curve, simply dot the resulting values with the
+    control points.
+
+    C(u) = [t^3, t^2, t, 1] * B_i * P, P = [P_i, P_{i+1}, P_{i+2}, P_{i+3}],
+
+    where P are the control points.
 
     Arguments:
         t : Parameter, `0 <= t <= 1`.
@@ -12,11 +21,11 @@ def basis(t):
 
 
     """
-    B = np.array([[1, -3, 3, -1],
-                  [4, 0, -6, 3],
-                  [1, 3, 3, -3],
-                  [0, 0, 0, 1]])/6
-    u = np.array([1, t, t**2, t**3])
+    B= np.array([[-1, 3, -3, 1],
+                 [3, -6, 3, 0],
+                 [-3, 0, 3, 0],
+                 [1, 4, 1, 0]])/6
+    u = np.array([t**3, t**2, t, 1])
 
-    out = B.dot(u)
+    out = u.dot(B)
     return out
--- splinefit/tests/test_bspline.py
+++ splinefit/tests/test_bspline.py
@@ -2,21 +2,40 @@ import splinefit as sf
 import matplotlib.pyplot as plt
 import numpy as np
 
-def test_basis():
+def test_cubic():
     n = 20
     s = np.linspace(0,1,n)
 
-    x = np.zeros((n,4))
     y = np.zeros((n,4))
 
     for i, si in enumerate(s):
-        z = sf.bspline.basis(si)
+        z = sf.bspline.cubic(si)
         for j in range(4):
-            x[i,j] = si+2 - j
             y[i,j] = z[j]
 
+    plt.plot(s,y)
+    #plt.show()
+
+def test_curve():
+    # Introduce one extra "ghost" control point at the end of the interval  
+    P = np.array([1.0, 1.0, 0.4, 0.0, 0.0, 0.0, 0.4, 1.0, 1.0])
+
+    n = 5
+    t = np.linspace(0, 1, n)
+    x = np.zeros(((len(P)-3)*n,))
+    y = np.zeros(((len(P)-3)*n,))
+    k = 0
+    for i in range(1,len(P)-2):
+        ctrlpts = P[i-1:i+3]
+        for ti in t:
+            qx = sf.bspline.cubic(ti).dot(ctrlpts)
+            y[k] = qx
+            x[k] = i + ti
+            k += 1
+    plt.clf()
     plt.plot(x,y)
     #plt.show()
+
 
 
 
