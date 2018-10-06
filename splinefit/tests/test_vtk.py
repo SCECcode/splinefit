6652771cb4329e7367cd1b95ad3e4161a968a538
ooreilly <ooreilly@stanford.edu>
2018-10-06 15:34:18 -0700

    Add vtk structured grid exporter.

--- splinefit/tests/test_vtk.py
+++ splinefit/tests/test_vtk.py
@@ -9,4 +9,12 @@ def load():
 
 def test_write_triangular_mesh():
     p, t = load()
-    vtk.write_triangular_mesh('fixtures/test.vtk', p[:,1:], t)
+    vtk.write_triangular_mesh('mesh/test.vtk', p[:,1:], t)
+
+def test_write_surface():
+    p, t = load()
+    x = np.linspace(0, 1, 10)
+    y = np.linspace(0, 1, 20)
+    X, Y = np.meshgrid(x, y)
+    Z = np.cos(0.4*np.pi*X)*np.cos(0.5*np.pi*Y)
+    vtk.write_surface('mesh/surf.vtk', X, Y, Z)
--- splinefit/vtk.py
+++ splinefit/vtk.py
@@ -16,11 +16,7 @@ def write_triangular_mesh(filename, p, t):
 
     f = open(filename, 'w')
 
-    header = \
-    "# vtk DataFile Version 3.0\n"\
-    + "vtk output\n"\
-    + "ASCII\n"\
-    + "DATASET UNSTRUCTURED_GRID\n"
+    header = write_header() + "DATASET UNSTRUCTURED_GRID\n"
     points = "POINTS %d float\n" % npts
     cells = "CELLS %d %d\n" % (ntris, 4*ntris)
     cell_types = "CELL_TYPES %d\n" % (ntris)
@@ -43,4 +39,41 @@ def write_triangular_mesh(filename, p, t):
     for i in range(ntris):
         f.write('%d\n'% cell_type)
 
+    f.close()
+
+def write_header():
+    header = "# vtk DataFile Version 3.0\n"\
+    + "vtk output\n"\
+    + "ASCII\n"
+    return header
+
+def write_surface(filename, X, Y, Z):
+    """
+    Write a Surface in 3D as structured grid in the VTK legacy file format.
+
+    Input arguments:
+        filename : Name of file to write to.
+        X : meshgrid of points in the x-direction 
+        Y : meshgrid of points in the y-direction 
+        Z : meshgrid of points in the z-direction 
+    """
+    assert X.shape[0] == Y.shape[0]
+    assert X.shape[0] == Z.shape[0]
+    assert X.shape[1] == Y.shape[1]
+    assert X.shape[1] == Z.shape[1]
+
+
+    f = open(filename, 'w')
+
+    header = write_header() + "DATASET STRUCTURED_GRID\n"
+    dim = "DIMENSIONS %d %d %d\n" %(X.shape[0], X.shape[1], 1)
+    points = "POINTS %d float\n" % (X.shape[0]*X.shape[1])
+
+    f.write(header)
+    f.write(dim)
+    f.write(points)
+    # x, y, z coordinates
+    for j in range(X.shape[1]):
+        for i in range(X.shape[0]):
+            f.write('%g %g %g\n' % (X[i,j], Y[i,j], Z[i,j]))
     f.close()
