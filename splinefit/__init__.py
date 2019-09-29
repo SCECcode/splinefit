e049c3099c1eefcee7fdfcc0baaa03848dd693ee
Ossian O'Reilly <ooreilly@usc.edu>
2019-09-28 17:38:08 -0700

    Update all command line tools.

--- splinefit/__init__.py
+++ splinefit/__init__.py
@@ -2,7 +2,7 @@ from . import vtk
 from . import triangulation
 from . import fitting
 from . import bspline
-from . import multilevel
+from . import msh
 from . import transfinite
 from . import options
 from . import plot
--- splinefit/options.py
+++ splinefit/options.py
@@ -1,15 +1,23 @@
 def get_options(args, delimiter="="):
     d_args = {}
+    nonoptions = []
     for arg in args[1::]:
-        key, value = arg.split(delimiter)
-        d_args[key] = value
+        try:
+            key, value = arg.split(delimiter)
+            d_args[key] = value
+        except:
+            nonoptions.append(arg)
+    d_args['args'] = nonoptions
+
     return d_args
 
 def check_options(args, options):
     args = get_options(args[1:])
     for arg in args:
-        if "--" in arg:
-            _arg = arg[2:]
+        if "-" in arg:
+            _arg = arg[1:]
+        elif "args" in arg:
+            continue
         else:
             _arg = arg
 
